eda16b499b610f5f7ae8be305c3b0b2c7b10d9e8
kimyurim <yfla980107@gmail.com>
2021-10-14 12:59:51 +0900

    211014 pybo_service

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -9,10 +9,12 @@ from .. import db
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
-# /question/list/
+# /question/list/?page=5
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 # /question/list/detail/<int:question_id>/
